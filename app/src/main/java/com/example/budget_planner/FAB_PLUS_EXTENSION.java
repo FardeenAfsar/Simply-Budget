da574720df8bb795f9e797b5552e14cd9613e778
Fardeen Afsar <afsarfardeen@gmail.com>
2020-08-08 16:57:49 -0400

    Added Transition from button

---
+++ app/src/main/java/com/example/budget_planner/FAB_PLUS_EXTENSION.java
@@ -0,0 +1,14 @@
+package com.example.budget_planner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FAB_PLUS_EXTENSION extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f_a_b__p_l_u_s__e_x_t_e_n_s_i_o_n);
+    }
+}
--- app/src/main/java/com/example/budget_planner/MainActivity.java
+++ app/src/main/java/com/example/budget_planner/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.budget_planner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton FabPlus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FabPlus =(FloatingActionButton)findViewById(R.id.fab_button);
+        FabPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openExtension();
+            }
+        });
+    }
+    public void openExtension(){
+        Intent intent = new Intent(this, FAB_PLUS_EXTENSION.class);
+        startActivity(intent);
     }
 }
