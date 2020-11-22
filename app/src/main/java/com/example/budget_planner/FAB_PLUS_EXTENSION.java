050061b1ce898c7ec0f890723eda7c88a8abf7d7
Fardeen Afsar <afsarfardeen@gmail.com>
2020-11-22 06:27:15 -0500

    Added Income/Expense UI

--- app/src/main/java/com/example/budget_planner/FAB_PLUS_EXTENSION.java
+++ app/src/main/java/com/example/budget_planner/FAB_PLUS_EXTENSION.java
@@ -7,12 +7,15 @@ import android.graphics.Color;
 import android.view.View;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 public class FAB_PLUS_EXTENSION extends AppCompatActivity {
     private boolean state = true;
+    private String[] incomeOptions = {"income_opt1", "income_opt2", "income_opt3", "income_opt4", "income_opt5"};
+    private String[] expenseOptions = {"expense_opt1", "expense_opt2", "expense_opt3", "expense_opt4", "expense_opt5", "expense_opt6", "expense_opt7"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +39,8 @@ public class FAB_PLUS_EXTENSION extends AppCompatActivity {
         incFab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#03DAC5")));
         expFab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#C8C8C8")));
         state = true;
+        enableButtons(incomeOptions);
+        disableButtons(expenseOptions);
     }
     public void expenseFab (View view){
         ExtendedFloatingActionButton expFab = (ExtendedFloatingActionButton) findViewById(R.id.expenseFab);
@@ -43,5 +48,24 @@ public class FAB_PLUS_EXTENSION extends AppCompatActivity {
         expFab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#03DAC5")));
         incFab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#C8C8C8")));
         state = false;
+        enableButtons(expenseOptions);
+        disableButtons(incomeOptions);
+    }
+
+    public void disableButtons(String[] buttonNames) {
+        for (String name : buttonNames) {
+            int id = getResources().getIdentifier(name, "id", getPackageName());
+            ExtendedFloatingActionButton button = (ExtendedFloatingActionButton) findViewById(id);
+            button.setEnabled(false);
+            button.hide();
+        }
+    }
+    public void enableButtons(String[] buttonNames) {
+        for (String name : buttonNames) {
+            int id = getResources().getIdentifier(name, "id", getPackageName());
+            ExtendedFloatingActionButton button = (ExtendedFloatingActionButton) findViewById(id);
+            button.setEnabled(true);
+            button.show();
+        }
     }
 }
