6e8e0cfe639311efe7c479cfd5a519405909afdb
Fardeen Afsar <afsarfardeen@gmail.com>
2020-11-23 07:01:58 -0500

    minor changes

--- app/src/main/java/com/example/budget_planner/FAB_PLUS_EXTENSION.java
+++ app/src/main/java/com/example/budget_planner/FAB_PLUS_EXTENSION.java
@@ -20,6 +20,7 @@ import java.util.Date;
 
 public class FAB_PLUS_EXTENSION extends AppCompatActivity {
     private boolean state = true;
+    private int storedPrevState = R.id.income_opt5;
     private String activeBtn = "Other";
     private String currentDate;
     private String[] incomeOptions = {"income_opt1", "income_opt2", "income_opt3", "income_opt4", "income_opt5"};
@@ -75,42 +76,70 @@ public class FAB_PLUS_EXTENSION extends AppCompatActivity {
         disableButtons(incomeOptions);
     }
     public void optionsBtn (View view){
+        deactivateButton(storedPrevState);
         switch (view.getId()) {
             case R.id.income_opt1:
                 activeBtn = "Allowance";
+                activateButton(R.id.income_opt1);
                 break;
             case R.id.income_opt2:
                 activeBtn = "Business Profits";
+                activateButton(R.id.income_opt2);
                 break;
             case R.id.income_opt3:
                 activeBtn = "Salary";
+                activateButton(R.id.income_opt3);
                 break;
             case R.id.income_opt4:
                 activeBtn = "Bonus";
+                activateButton(R.id.income_opt4);
+                break;
+            case R.id.income_opt5:
+                activeBtn = "Other";
+                activateButton(R.id.income_opt5);
                 break;
             case R.id.expense_opt1:
                 activeBtn = "Food";
+                activateButton(R.id.expense_opt1);
                 break;
             case R.id.expense_opt2:
                 activeBtn = "Transportation";
+                activateButton(R.id.expense_opt2);
                 break;
             case R.id.expense_opt3:
                 activeBtn = "Social Life";
+                activateButton(R.id.expense_opt3);
                 break;
             case R.id.expense_opt4:
                 activeBtn = "Bills";
+                activateButton(R.id.expense_opt4);
                 break;
             case R.id.expense_opt5:
                 activeBtn = "Health";
+                activateButton(R.id.expense_opt5);
                 break;
             case R.id.expense_opt6:
                 activeBtn = "Gifts";
+                activateButton(R.id.expense_opt6);
                 break;
-            case R.id.income_opt5:
             case R.id.expense_opt7:
                 activeBtn = "Other";
+                activateButton(R.id.expense_opt7);
                 break;
         }
+    }
+
+
+    public void activateButton(int id) {
+        ExtendedFloatingActionButton switchbtn = (ExtendedFloatingActionButton) findViewById(id);
+        switchbtn.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#6200EE")));
+        switchbtn.setTextColor(Color.parseColor("#FFFFFF"));
+        storedPrevState = id;
+    }
+    public void deactivateButton(int id) {
+        ExtendedFloatingActionButton switchbtn = (ExtendedFloatingActionButton) findViewById(id);
+        switchbtn.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#C8C8C8")));
+        switchbtn.setTextColor(Color.parseColor("#000000"));
     }
 
     public void disableButtons(String[] buttonNames) {
